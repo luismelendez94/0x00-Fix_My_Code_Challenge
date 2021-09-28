c5a43fcd8f9bb4ab4b987bb38741f57e90ce8977
luismelendez94 <2709@holbertonschool.com>
2021-09-28 13:47:17 +0000

    Task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 class square():
 
     width = 0
     height = 0
-
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -15,7 +15,7 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return (self.width + self.height) * 2
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
