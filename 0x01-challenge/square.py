66db7a8228d0b7405c6e1b36d346fa797aa21d84
luismelendez94 <2709@holbertonschool.com>
2021-10-05 04:10:30 +0000

    Fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" File that show calculus of a square """
 
 
 class Square():
+    """ Define class Square """
 
     def __init__(self, width, height):
+        """ Initialite variables """
         self.width = width
         self.height = height
 
@@ -12,9 +15,11 @@ class Square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width + self.height) * 2
 
     def __str__(self):
+        """ Print format """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
